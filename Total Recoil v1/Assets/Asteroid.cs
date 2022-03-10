b6f65fbebd60a2e16ee7b9c40515f01b7f9e1625
Daniel Gutierrez <83122636+PufferMcSparkleFace@users.noreply.github.com>
2022-03-10 23:04:55 +1300

    Asteroid Prefab

--- Total Recoil v1/Assets/Asteroid.cs
+++ Total Recoil v1/Assets/Asteroid.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Asteroid : MonoBehaviour
 {
+    private SpriteRenderer spriteRenderer;
+    private Rigidbody2D rb;
+    public Sprite[] sprites;
+    public float size = 5.0f;
+    public float minSize = 3.0f;
+    public float maxSize = 15.0f;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //randomising the sprite of the asteroid
+        spriteRenderer.sprite = sprites[Random.Range(0, sprites.Length)];
+
+        //randomizing the rotation of the asteroids
+        this.transform.eulerAngles = new Vector3(0.0f, 0.0f, Random.value * 360.0f);
+
+       //vector3.one is shorthand for new vector3 = (this.size, this.size, this.size)
+        this.transform.localScale = Vector3.one * this.size;
+
+        rb.mass = this.size;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
