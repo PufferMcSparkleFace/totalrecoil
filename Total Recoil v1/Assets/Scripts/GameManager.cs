c9aa2a0f540318670726f66ae2d174e3fad3fbba
Daniel Gutierrez <83122636+PufferMcSparkleFace@users.noreply.github.com>
2022-03-14 18:40:57 +1300

    resetting

--- Total Recoil v1/Assets/Scripts/GameManager.cs
+++ Total Recoil v1/Assets/Scripts/GameManager.cs
@@ -7,9 +7,9 @@ public class GameManager : MonoBehaviour
     public Player playerScript;
     public GameObject playerObject;
     public float health;
-    public float maxHealth = 1f;
-    public float healRate = 0.001f;
-    public float healRateWhileShooting = 0.0005f;
+    public float maxHealth = 100f;
+    public float healRate = 0.1f;
+    public float healRateWhileShooting = 0.05f;
     public bool isHealing;
     public float healTime = 3.0f;
     public Rigidbody2D rb;
