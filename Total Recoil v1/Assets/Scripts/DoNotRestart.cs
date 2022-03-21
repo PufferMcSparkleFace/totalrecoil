d1c134fb761975d6f60eb5893d082047c843719d
Daniel Gutierrez <83122636+PufferMcSparkleFace@users.noreply.github.com>
2022-03-21 18:53:08 +1300

    Start Menu

---
+++ Total Recoil v1/Assets/Scripts/DoNotRestart.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoNotRestart : MonoBehaviour
+{
+    public AudioSource bgm;
+
+    private void Awake()
+    {
+        GameObject currentBGM = GameObject.FindGameObjectWithTag("GameMusic");
+        if(currentBGM == null)
+        {
+            AudioSource spawned = Instantiate(bgm);
+            spawned.Play();
+            DontDestroyOnLoad(spawned);
+        }
+    }
+}
