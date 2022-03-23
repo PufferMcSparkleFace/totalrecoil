1e3df2a2c9153e762158f6a386060713932a87bc
Daniel Gutierrez <83122636+PufferMcSparkleFace@users.noreply.github.com>
2022-03-23 23:39:50 +1300

    Homing Enemy v1

---
+++ Total Recoil v1/Assets/Scripts/HomingEnemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomingEnemy : MonoBehaviour
+{
+    public float thrustSpeed = 2f;
+    public Rigidbody2D rb;
+    public GameObject target;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        target = GameObject.FindGameObjectWithTag("Target");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce((target.transform.position - this.transform.position) * thrustSpeed);
+    }
+}
